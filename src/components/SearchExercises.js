08b722b6c957978b551515e1c9b8a0ab664f3034
JCOCHARLIE <lawrieabuna@gmail.com>
2024-04-14 23:42:54 +0300

    Added the categories component and finished the functionality for the search function using the exerceise db api from Rapid API

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,14 +2,37 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
 
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('')
+  const [bodyParts, setBodyParts] = useState([])
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisebd.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+
+    fetchExercisesData();
+  }, [])
 
   const handleSearch = async () => {
     if(search) {
-      //const exercisesData = await fetchData();
+      const exercisesData = await fetchData('https://exercisebd.p.rapidapi.com/exercises', exerciseOptions);
+
+      const searchedExercises = exercisesData.filter(
+        (exercise) => exercise.name.toLowerCase().includes(search)
+        ||exercise.target.toLowerCase().includes(search)
+        ||exercise.bodyPart.toLowerCase().includes(search)
+        ||exercise.equipment.toLowerCase().includes(search)
+      );
+
+      setSearch('');
+      setExercises(searchedExercises);
     }
   }
   return (
@@ -46,6 +69,10 @@ const SearchExercises = () => {
           }}
           onClick={handleSearch}
         >Search</Button>
+      </Box>
+      <Box sx={{position:'relative', width: '100%', p:'20px'}}>
+        <HorizontalScrollbar data={bodyParts}
+        bodyPart={bodyPart} setBodyPart={setBodyPart}/>
       </Box>
     </Stack>
   )
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,14 @@
+export const exerciseOptions = {
+    method: 'GET',
+  params: {limit: '10'},
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
 export const fetchData = async (url, options) => {
-    const responsem= await fetch(url,options);
-    const data = await responsem.json();
+    const response= await fetch(url,options);
+    const data = await response.json();
 
     return data;
 }
